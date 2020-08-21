aac7872944745575e16e1c539d385101b75e5050
DhavanSinghal <dhavansinghal@gmail.com>
2020-08-21 22:50:19 +0530

    initial Setup

---
+++ src/MyTable.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyTable extends Component {
+    state = {  }
+
+    render() { 
+        return {
+            
+        };
+    }
+}
+ 
+export default MyTable;
